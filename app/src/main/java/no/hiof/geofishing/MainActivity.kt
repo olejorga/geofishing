dbe7a495bbb06922e1103708a9d445018f698e14
simenyo <simen.90@gmail.com>
2022-09-29 13:49:48 +0200

    fix: New theme and added comments to navigation component setup

--- app/src/main/java/no/hiof/geofishing/MainActivity.kt
+++ app/src/main/java/no/hiof/geofishing/MainActivity.kt
@@ -11,26 +11,40 @@ import no.hiof.geofishing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    // Setup of navController since this is the navHost class
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setSupportActionBar(binding.toolbar)
 
+        // Using the Navigation component(nav_graph, NavHostFragment, NavController)
+        // Set the navHostFragment to the id of FragmentContainer(navHost) from activity_main.xml
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        // navController for the navHostFragment
         navController = navHostFragment.navController
 
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.mapsFragment, R.id.feedFragment, R.id.catchFragment, R.id.rankFragment, R.id.userPageFragment))
+        // The setOf top-destination fragments of the app so the Up-button is not displayed when
+        // navHost is at a top-level fragment
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.mapsFragment,
+                R.id.feedFragment,
+                R.id.catchFragment,
+                R.id.rankFragment,
+                R.id.userPageFragment
+            )
+        )
 
+        // Finish setup of toolbar with navController and config
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
 
-        val navView : BottomNavigationView = binding.bottomNavigation
-
-        //val navController = navHostFragment.navController
-
+        // setting up the binding and navController for bottomNavigation
+        val navView: BottomNavigationView = binding.bottomNavigation
         navView.setupWithNavController(navController)
     }
 }
