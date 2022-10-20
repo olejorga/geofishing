7914f6f31c8168c857204b720f550b0d900474e5
simenyo <simen.90@gmail.com>
2022-10-20 17:06:31 +0200

    FEAT: Maps now passes location when a new catch is created, and sets a marker on this location, moved some logic out to MapViewModel.kt

--- app/src/main/java/no/hiof/geofishing/ui/viewmodels/MapViewModel.kt
+++ app/src/main/java/no/hiof/geofishing/ui/viewmodels/MapViewModel.kt
@@ -1,8 +1,37 @@
 package no.hiof.geofishing.ui.viewmodels
 
+import android.Manifest
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import no.hiof.geofishing.MainActivity
 import no.hiof.geofishing.data.contracts.AuthService
+import no.hiof.geofishing.data.contracts.Repository
+import no.hiof.geofishing.data.contracts.Response
+import no.hiof.geofishing.data.entities.Catch
 
-class MapViewModel(authService: AuthService) : ViewModel() {
+class MapViewModel(catchRepository: Repository<Catch>) : ViewModel() {
+    val locationPermissionsRequired = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    lateinit var catchList : LiveData<Response<List<Catch>>>
+
+    init {
+        viewModelScope.launch {
+            catchList = catchRepository.read().asLiveData()
+        }
+
+
+    }
+
+    fun setLocation(lat: Double, long: Double) {
+        MainActivity.latitude = lat
+        MainActivity.longitude = long
+    }
+
 
 }
