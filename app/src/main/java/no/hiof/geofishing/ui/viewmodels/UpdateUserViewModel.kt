538b5ba07485d85fd9fc8d98c6f0ef12e7d952fd
Erik Jarem <erik.jarem@gmail.com>
2022-10-21 11:00:31 +0200

    Feature: added email, password and confirm password fields to update profile

--- app/src/main/java/no/hiof/geofishing/ui/viewmodels/UpdateUserViewModel.kt
+++ app/src/main/java/no/hiof/geofishing/ui/viewmodels/UpdateUserViewModel.kt
@@ -1,7 +1,19 @@
 package no.hiof.geofishing.ui.viewmodels
 
+import android.provider.ContactsContract.Profile
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import no.hiof.geofishing.data.contracts.Repository
+import no.hiof.geofishing.data.contracts.Response
 
-class UpdateUserViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class UpdateUserViewModel(profileRepository: Repository<Profile>) : ViewModel() {
+    lateinit var profile: LiveData<Response<Profile>>
+
+    init {
+        viewModelScope.launch {
+
+        }
+    }
 }
