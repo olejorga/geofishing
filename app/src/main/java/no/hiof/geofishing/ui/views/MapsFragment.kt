3c705b74bd22ec77f2d33993388e4877c54cb3f2
simenyo <simen.90@gmail.com>
2022-10-25 01:52:05 +0200

    FIX: No more duplicate data on markers. split nav_graph.xml into specific parts.

--- app/src/main/java/no/hiof/geofishing/ui/views/MapsFragment.kt
+++ app/src/main/java/no/hiof/geofishing/ui/views/MapsFragment.kt
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -46,6 +45,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
     private var mGoogleMap: GoogleMap? = null
     private var offsetBy = 0
 
+    // Constant determining offset on duplicate marker LatLngs
     companion object {
         private const val POSITION_OFFSET = 0.000085f
     }
@@ -58,7 +58,7 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
         }
     }
 
-    // Get the string-array of species from strings.xml
+    // Get an array of species from strings.xml and the species marker picture id's from drawable
     override fun onAttach(context: Context) {
         super.onAttach(context)
         fishSpeciesArray = context.resources.getStringArray(R.array.fish_array)
@@ -80,8 +80,8 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Gets the map async, callback = onMapReady
 
+        // Gets the map async, callback = onMapReady
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
@@ -131,11 +131,13 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
         if (mGoogleMap == null)
             mGoogleMap = googleMap
 
+        // Starts the process of permission checks
         checkLocationPermissions()
 
+        // Checks view model list against Db then places markers
         viewModel.catchList.observe(viewLifecycleOwner) { response ->
             if (response.error == null && response.data != null) {
-                if (viewModel.catchList2 != response.data)
+                if (viewModel.catchList2 != response.data) {
                     viewModel.catchList2 = response.data as MutableList<Catch>
 
                     viewModel.catchList2.forEachIndexed { index, catch ->
@@ -149,20 +151,19 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
                             marker?.tag = index.toString()
                         }
                     }
-
+                }
             }
-
         }
         googleMap.setOnMarkerClickListener(this)
     }
 
-    // passing along marker.tag to deeplink for arg in FeedPostDetailFragment.kt
+    // parses a uri with an argument from marker to show detail view of clicked catch on map
     override fun onMarkerClick(marker: Marker): Boolean {
         val uri = Uri.parse("myapp://Geofishing.com/${marker.tag}")
         if (findNavController().graph.hasDeepLink(uri)) {
             findNavController().navigate(uri)
         } else {
-            Log.d("DEEPLINK EXISTN'T", findNavController().graph.hasDeepLink(uri).toString())
+            // TODO toast?
         }
         return true
     }
@@ -170,14 +171,15 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
     // Gets the int id's of drawable resources
     private fun getDrawableIntIds(): IntArray {
         val toArray = resources.obtainTypedArray(R.array.icons)
-        val ids = IntArray(toArray.length())
-        for (i in ids.indices)
-            ids[i] = toArray.getResourceId(i, 0)
+        val drawableIntIds = IntArray(toArray.length())
+        for (i in drawableIntIds.indices)
+            drawableIntIds[i] = toArray.getResourceId(i, 0)
 
         toArray.recycle()
-        return ids
+        return drawableIntIds
     }
 
+    // Creates a marker with a position and a marker icon
     private fun createMapMarker(markerPosition: LatLng, markerIcon: BitmapDescriptor?): Marker? {
         val uniquePosition = checkMarkerOverLap(markerPosition)
         return mGoogleMap?.addMarker(
@@ -185,6 +187,28 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
                 .position(uniquePosition)
                 .icon(markerIcon)
         )
+    }
+
+    // Sets the current position on map
+    @SuppressLint("MissingPermission")
+    private fun setUserLocation() {
+        mGoogleMap?.isMyLocationEnabled = true
+        fusedLocationProviderClient.getCurrentLocation(
+            100, null
+        ).addOnSuccessListener { location: Location? ->
+            val currentLocationLatLng =
+                LatLng(
+                    location?.latitude ?: viewModel.defLatitude,
+                    location?.longitude ?: viewModel.defLongitude
+                )
+            mGoogleMap?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    currentLocationLatLng,
+                    17.0f
+                )
+            )
+            viewModel.setLocation(currentLocationLatLng)
+        }
     }
 
     // Checks if permissions are granted, if not starts a new req, else set user location.
@@ -235,27 +259,6 @@ class MapsFragment : Fragment(), OnMapReadyCallback, OnMarkerClickListener {
                 }
             }
         }
-
-    @SuppressLint("MissingPermission")
-    private fun setUserLocation() {
-        mGoogleMap?.isMyLocationEnabled = true
-        fusedLocationProviderClient.getCurrentLocation(
-            100, null
-        ).addOnSuccessListener { location: Location? ->
-            val currentLocationLatLng =
-                LatLng(
-                    location?.latitude ?: viewModel.defLatitude,
-                    location?.longitude ?: viewModel.defLongitude
-                )
-            mGoogleMap?.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(
-                    currentLocationLatLng,
-                    17.0f
-                )
-            )
-            viewModel.setLocation(currentLocationLatLng)
-        }
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
